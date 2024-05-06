c14f6a7196cb9c8bc2bd85eae45ddf568db8a5e0
katungi <dankatdennis@gmail.com>
2024-05-06 14:54:26 +0300

    feat: finished with the video calls

--- src/app/app/(authenticated-routes)/(user-scope)/meeting/[id]/MeetingComponent.tsx
+++ src/app/app/(authenticated-routes)/(user-scope)/meeting/[id]/MeetingComponent.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import { Call, CallControls, SpeakerLayout, StreamCall, StreamTheme, useCallStateHooks, useStreamVideoClient } from "@stream-io/video-react-sdk"
+import { Call, CallControls, CallingState, DeviceSettings, SpeakerLayout, StreamCall, StreamTheme, VideoPreview, useCallStateHooks, useStreamVideoClient } from "@stream-io/video-react-sdk"
 import { Loader } from "lucide-react"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 import { Button } from "~/components/ui/button"
 import useLoadCall from "~/hooks/use-load-call"
 import useStreamCall from "~/hooks/use-stream-call"
+import PermissionPrompt from "./PermissionPrompt"
+import AudioVolumeIndicator from "./AudioVolumeIndicator"
+import FlexibleCallLayout from "./FlexibleCallLayout"
 
 interface MeetingPageProps {
     id: string
@@ -36,11 +39,18 @@ export default function MeetingPageComponent({ id }: MeetingPageProps) {
     )
 }
 function MeetingScreen() {
+    const [setupComplete, setSetupComplete] = useState(false)
     const { useCallEndedAt, useCallStartedAt } = useCallStateHooks()
+
+    const call = useStreamCall()
 
     const callEndedAt = useCallEndedAt()
     const callStartAt = useCallStartedAt()
 
+    async function handleSetupComplete() {
+        call.join()
+        setSetupComplete(true)
+    }
     const callIsInFuture = callStartAt && new Date(callStartAt) > new Date();
 
     const callHasEnded = !!callEndedAt;
@@ -52,14 +62,81 @@ function MeetingScreen() {
     if (callIsInFuture) {
         return <UpcomingMeetingScreen />
     }
+    const description = call.state.custom.description;
 
-    return <div>Call UI</div>
+    return <div className="space-y-6">
+        <h1 className="text-2xl font-bold">Meeting</h1>
+        {description && <p className="text-xl">{description}</p>}
+        {setupComplete ? (
+            <SpeakerLayout />
+        ) : (
+            <SetupUI onSetupComplete={handleSetupComplete} />
+        )}
+    </div>
 }
 
+interface SetupUIProps {
+    onSetupComplete: () => void
+}
+function SetupUI({ onSetupComplete }: SetupUIProps) {
+    const call = useStreamCall();
+
+    const { useMicrophoneState, useCameraState } = useCallStateHooks();
+
+    const micState = useMicrophoneState();
+    const camState = useCameraState();
+
+    const [micCamDisabled, setMicCamDisabled] = useState(false);
+
+    useEffect(() => {
+        if (micCamDisabled) {
+            call.camera.disable();
+            call.microphone.disable();
+        } else {
+            call.camera.enable();
+            call.microphone.enable();
+        }
+    }, [micCamDisabled, call]);
+
+    if (!micState.hasBrowserPermission || !camState.hasBrowserPermission) {
+        return <PermissionPrompt />;
+    }
+
+    return (
+        <div className="flex flex-col items-center gap-3">
+            <h1 className="text-center text-2xl font-bold">Setup</h1>
+            <VideoPreview />
+            <div className="flex h-16 items-center gap-3">
+                <AudioVolumeIndicator />
+                <DeviceSettings />
+            </div>
+            <label className="flex items-center gap-2 font-medium">
+                <input
+                    type="checkbox"
+                    checked={micCamDisabled}
+                    onChange={(e) => setMicCamDisabled(e.target.checked)}
+                />
+                Join with mic and camera off
+            </label>
+            <Button onClick={onSetupComplete}>Join meeting</Button>
+        </div>
+    );
+}
+
+function CallUI() {
+    const { useCallCallingState } = useCallStateHooks();
+  
+    const callingState = useCallCallingState();
+  
+    if (callingState !== CallingState.JOINED) {
+      return <Loader className="mx-auto animate-spin" />;
+    } 
+  
+    return <FlexibleCallLayout />;
+  }
+  
 function UpcomingMeetingScreen() {
-
     const call = useStreamCall()
-
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <p className="text-2xl">Meeting starts soon - {" "}
---
+++ src/app/app/(authenticated-routes)/(user-scope)/meeting/[id]/left/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+
+interface PageProps {
+    params: { id: string };
+}
+
+export default function Page({ params: { id } }: PageProps) {
+    return (
+        <div className="flex flex-col items-center gap-3">
+            <p className="font-bold">You left this meeting.</p>
+            <Link
+                href={`/app/meeting/${id}`}
+                className="bg-gray-500 hover:bg-gray-600"
+            >
+                <Button>Rejoin</Button>
+            </Link>
+        </div>
+    );
+}
